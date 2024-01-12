076399064c8e2f1acd3657c33cc86bde2b4f59a8
Hunter Carlis <huntercarlis@yahoo.com>
2024-01-12 11:01:43 -0800

    new avitar

--- Portfolio/src/components/AboutMe.tsx
+++ Portfolio/src/components/AboutMe.tsx
@@ -9,6 +9,7 @@ import {
   Center,
 } from "@chakra-ui/react";
 import ColorModeButton from "./ColorModeButton";
+import avitar from "../assets/avitar.jpg";
 
 export const AboutMe = () => {
   return (
@@ -22,10 +23,11 @@ export const AboutMe = () => {
         <Stack>
           <Center>
             <Image
+              borderRadius="full"
               objectFit="cover"
-              maxW={{ base: "100%", sm: "200px", md: "300px", lg: "500px" }}
-              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-              alt="Caffe Latte"
+              maxW="180px"
+              src={avitar}
+              alt="Avitar"
             />
           </Center>
           <CardBody>
